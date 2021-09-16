d28872c98ce00465a1549c9a088451f57b375c85
Emmanuelle Gouillart <emmanuelle.gouillart@gmail.com>
2021-09-16 08:54:29 +0200

    added diffusion matrices

---
+++ diffusion_values.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+diffusion_dict = {
+        'BNS':{'eigvals':[10.1, 0.5], 
+                 'eigvecs':np.matrix([[-0.1, 0.75], [1, 0.25]])},
+        'NCS':{'eigvals':[105, 4.4], 
+                 'eigvecs':np.matrix([[1, 0.27], [-0.85, 0.73]])},
+        }
